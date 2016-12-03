2bc3b217458e05d0c3a562ba84c6fe554794e606
Mikael Hellqvist <mikaelhellqvist@Mikaels-MacBook-Pro.local>
2016-12-03 08:16:29 +0100

    Initial Commit

---
+++ AdventOfCode1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdventOfCode1
+//
+//  Created by Mikael Hellqvist on 2016-12-03.
+//  Copyright © 2016 Mikael Hellqvist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
